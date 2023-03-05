03550b0e9383bd85397817f236e2ce8e58348d8d
michioxd <michio.haiyaku@gmail.com>
2023-03-05 18:31:56 +0700

    init commit

--- src/mainCss.tsx
+++
@@ -1,4 +0,0 @@
-// minified CSS from ripple.css
-const mainCss: string = /** @noinline */ `/*react-ripplejs*/.rippleJS,.rippleJS.fill::after{top:0;left:0;right:0;bottom:0;position:absolute}.rippleJS{display:block;overflow:hidden;border-radius:inherit;-webkit-mask-image:-webkit-radial-gradient(circle,white,black)}.rippleJS.fill::after{content:""}.rippleJS.fill{border-radius:1000000px}.rippleJS .ripple{position:absolute;border-radius:100%;background:currentColor;opacity:.2;width:0;height:0;-webkit-transition:-webkit-transform .5s ease-out,opacity .5s ease-out;transition:transform .5s ease-out,opacity .5s ease-out;-webkit-transform:scale(0);transform:scale(0);pointer-events:none;-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}.rippleJS .ripple.held{opacity:.1;-webkit-transform:scale(1);transform:scale(1)}.rippleJS .ripple.done{opacity:0}`;
-
-export default mainCss;
